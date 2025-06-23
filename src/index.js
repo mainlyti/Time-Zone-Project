ec56d220dd59ef1e9bfce55b562bb444d8f06dc8
mainlyti <mainlyti@gmail.com>
2025-06-23 02:48:51 -0400

    added js

--- src/index.js
+++ src/index.js
@@ -3,4 +3,9 @@ let newYorkElement = document.querySelector("#newYork");
 let newYorkDateElement = newYorkElement.querySelector(".date");
 let newYorkTimeElement = newYorkElement.querySelector(".time");
 
-newYorkDateElement.innerHTML = moment().format("dddd MMMM D, YYYY");
+newYorkDateElement.innerHTML = moment()
+  .tz("America/New_York")
+  .format("dddd MMMM D, YYYY");
+newYorkTimeElement.innerHTML = moment()
+  .tz("America/New_York")
+  .format("h:mm:ss [<small>]A[</small>]");
