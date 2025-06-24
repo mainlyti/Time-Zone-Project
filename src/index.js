c149a5ccd53ab8146a24e07a6eeb290044edf3d2
mainlyti <mainlyti@gmail.com>
2025-06-24 02:35:51 -0400

    added time zones to other options and have them display

--- src/index.js
+++ src/index.js
@@ -13,5 +13,23 @@ function updateTime() {
   }
 }
 
+function updateCity(event) {
+  let cityTimeZone = event.target.value;
+  let cityName = cityTimeZone.replace("_", " ").split("/")[1];
+  let cityTime = moment().tz(cityTimeZone);
+  let cityElement = document.querySelector("#allDisplayedCities");
+  cityElement.innerHTML = `
+  <div class="currentCity">
+          <h2>${cityName}</h2>
+          <div class="currentTime">${cityTime.format(
+            "h:mm:ss [<small>]A[</small>]"
+          )}</div>
+          <div class="currentDate">${cityTime.format("dddd MMMM D, YYYY")}</div>
+        </div>`;
+}
+
 updateTime();
 setInterval(updateTime, 1000);
+
+let citySelectElement = document.querySelector("#city");
+citySelectElement.addEventListener("change", updateCity);
