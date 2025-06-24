1bcdc5413e9e7d55deec4357499cd40269498393
mainlyti <mainlyti@gmail.com>
2025-06-24 05:35:58 -0400

    changed around js to make main page display local time and date and fixed other errors

--- src/index.js
+++ src/index.js
@@ -1,20 +1,46 @@
 function updateTime() {
+  let currentCityElement = document.querySelector("h2");
+  let currentCityTimeElement = document.querySelector(".currentTime");
+  let currentCityDateElement = document.querySelector(".currentDate");
+  let currentCity = moment.tz.guess();
+  let currentCityDate = moment.tz(currentCity);
+
+  currentCityElement.innerHTML = currentCity.replace("_", " ").split("/")[1];
+  currentCityTimeElement.innerHTML = currentCityDate.format(
+    "h:mm:ss [<small>]a[</small>]"
+  );
+  currentCityDateElement.innerHTML =
+    currentCityDate.format("dddd MMMM D, YYYY");
+
+  //London
+  let londonElement = document.querySelector("#london");
+  let londonTimeElement = londonElement.querySelector("#london-time");
+  let londonTime = moment().tz("Europe/London");
+
+  londonTimeElement.innerHTML = londonTime.format("h:mm [<small>]a[</small>]");
+
+  //Tokyo
+  let tokyoElement = document.querySelector("#tokyo");
+  let tokyoTimeElement = tokyoElement.querySelector("#tokyo-time");
+  let tokyoTime = moment().tz("Asia/Tokyo");
+
+  tokyoTimeElement.innerHTML = tokyoTime.format("h:mm [<small>]a[</small>]");
+
   //New York
   let newYorkElement = document.querySelector("#new-york");
-  if (newYorkElement) {
-    let newYorkDateElement = newYorkElement.querySelector(".currentDate");
-    let newYorkTimeElement = newYorkElement.querySelector(".currentTime");
-    let newYorkTime = moment().tz("America/New_York");
+  let newYorkTimeElement = newYorkElement.querySelector("#new-york-time");
+  let newYorkTime = moment().tz("America/New_York");
 
-    newYorkDateElement.innerHTML = newYorkTime.format("dddd MMMM D, YYYY");
-    newYorkTimeElement.innerHTML = newYorkTime.format(
-      "h:mm:ss [<small>]A[</small>]"
-    );
-  }
+  newYorkTimeElement.innerHTML = newYorkTime.format(
+    "h:mm [<small>]a[</small>]"
+  );
 }
 
 function updateCity(event) {
   let cityTimeZone = event.target.value;
+  if (cityTimeZone === "current") {
+    cityTimeZone = moment.tz.guess();
+  }
   let cityName = cityTimeZone.replace("_", " ").split("/")[1];
   let cityTime = moment().tz(cityTimeZone);
   let cityElement = document.querySelector("#allDisplayedCities");
@@ -22,7 +48,7 @@ function updateCity(event) {
   <div class="currentCity">
           <h2>${cityName}</h2>
           <div class="currentTime">${cityTime.format(
-            "h:mm:ss [<small>]A[</small>]"
+            "h:mm:ss [<small>]a[</small>]"
           )}</div>
           <div class="currentDate">${cityTime.format("dddd MMMM D, YYYY")}</div>
         </div>`;
